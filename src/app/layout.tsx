5b8fca76d116f5fb1baf3a413cd24d36b6b94321
MasudShishir222 <masudeshishir222@gmail.com>
2026-09-26 14:38:28 +0600

    Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col bg-black">
+      <body className="min-h-full flex flex-col bg-black" cz-shortcut-listen="true">
         <Navbar />
         {children}
         <Footer/>
